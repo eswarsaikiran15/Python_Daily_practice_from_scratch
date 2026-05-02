e0dcc231a07443251b005d2cce348d5e29c9f9b0
K.ESWAR SAI KIRAN <160008710+eswarsaikiran15@users.noreply.github.com>
2026-05-02 21:38:55 +0530

    Add files via upload

---
+++ DSA_Practice_from_basic_to_advanced/4_prime.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+
+# Input + Output
+n = int(input("Enter a number: "))
+
+if is_prime(n):
+    print("Prime number")
+else:
+    print("Not a prime number")
